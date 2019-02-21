ee5a4836bae2ddd98812d052a158719de1ddec9d
Kiran A <33511457+codesbyk@users.noreply.github.com>
2019-02-21 19:04:38 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { GraphQLServer } from "graphql-yoga";
+import uuidv4 from "uuid/v4";
 
 // We have the following Scalar Types below
 // -------> String, Boolean, Int, Float, ID
 
 // Demo User Data
-var dummyUsers = [
+const dummyUsers = [
   {
     id: 1,
     name: "Leanne Graham",
@@ -95,7 +96,7 @@ const dummyPosts = [
   },
   {
     userId: 2,
-    id: 11,
+    id: 4,
     title: "et ea vero quia laudantium autem",
     body:
       "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi",
@@ -103,7 +104,7 @@ const dummyPosts = [
   },
   {
     userId: 2,
-    id: 12,
+    id: 28,
     title: "in quibusdam tempore odit est dolorem",
     body:
       "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio",
@@ -111,7 +112,7 @@ const dummyPosts = [
   },
   {
     userId: 2,
-    id: 20,
+    id: 5,
     title: "doloribus ad provident suscipit at",
     body:
       "qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo",
@@ -119,7 +120,7 @@ const dummyPosts = [
   },
   {
     userId: 3,
-    id: 29,
+    id: 6,
     title: "iusto eius quod necessitatibus culpa ea",
     body:
       "odit magnam ut saepe sed non qui\ntempora atque nihil\naccusamus illum doloribus illo dolor\neligendi repudiandae odit magni similique sed cum maiores",
@@ -127,7 +128,7 @@ const dummyPosts = [
   },
   {
     userId: 3,
-    id: 30,
+    id: 7,
     title: "a quo magni similique perferendis",
     body:
       "alias dolor cumque\nimpedit blanditiis non eveniet odio maxime\nblanditiis amet eius quis tempora quia autem rem\na provident perspiciatis quia",
@@ -135,7 +136,7 @@ const dummyPosts = [
   },
   {
     userId: 4,
-    id: 38,
+    id: 8,
     title: "explicabo et eos deleniti nostrum ab id repellendus",
     body:
       "animi esse sit aut sit nesciunt assumenda eum voluptas\nquia voluptatibus provident quia necessitatibus ea\nrerum repudiandae quia voluptatem delectus fugit aut id quia\nratione optio eos iusto veniam iure",
@@ -143,7 +144,7 @@ const dummyPosts = [
   },
   {
     userId: 4,
-    id: 39,
+    id: 9,
     title: "eos dolorem iste accusantium est eaque quam",
     body:
       "corporis rerum ducimus vel eum accusantium\nmaxime aspernatur a porro possimus iste omnis\nest in deleniti asperiores fuga aut\nvoluptas sapiente vel dolore minus voluptatem incidunt ex",
@@ -151,7 +152,7 @@ const dummyPosts = [
   },
   {
     userId: 4,
-    id: 40,
+    id: 10,
     title: "enim quo cumque",
     body:
       "ut voluptatum aliquid illo tenetur nemo sequi quo facilis\nipsum rem optio mollitia quas\nvoluptatem eum voluptas qui\nunde omnis voluptatem iure quasi maxime voluptas nam",
@@ -159,7 +160,7 @@ const dummyPosts = [
   },
   {
     userId: 5,
-    id: 41,
+    id: 11,
     title: "non est facere",
     body:
       "molestias id nostrum\nexcepturi molestiae dolore omnis repellendus quaerat saepe\nconsectetur iste quaerat tenetur asperiores accusamus ex ut\nnam quidem est ducimus sunt debitis saepe",
@@ -167,7 +168,7 @@ const dummyPosts = [
   },
   {
     userId: 5,
-    id: 42,
+    id: 12,
     title:
       "commodi ullam sint et excepturi error explicabo praesentium voluptas",
     body:
@@ -176,7 +177,7 @@ const dummyPosts = [
   },
   {
     userId: 6,
-    id: 51,
+    id: 13,
     title: "soluta aliquam aperiam consequatur illo quis voluptas",
     body:
       "sunt dolores aut doloribus\ndolore doloribus voluptates tempora et\ndoloremque et quo\ncum asperiores sit consectetur dolorem",
@@ -184,7 +185,7 @@ const dummyPosts = [
   },
   {
     userId: 6,
-    id: 52,
+    id: 14,
     title: "qui enim et consequuntur quia animi quis voluptate quibusdam",
     body:
       "iusto est quibusdam fuga quas quaerat molestias\na enim ut sit accusamus enim\ntemporibus iusto accusantium provident architecto\nsoluta esse reprehenderit qui laborum",
@@ -192,7 +193,7 @@ const dummyPosts = [
   },
   {
     userId: 7,
-    id: 61,
+    id: 15,
     title: "voluptatem doloribus consectetur est ut ducimus",
     body:
       "ab nemo optio odio\ndelectus tenetur corporis similique nobis repellendus rerum omnis facilis\nvero blanditiis debitis in nesciunt doloribus dicta dolores\nmagnam minus velit",
@@ -200,7 +201,7 @@ const dummyPosts = [
   },
   {
     userId: 8,
-    id: 71,
+    id: 16,
     title: "et iusto veniam et illum aut fuga",
     body:
       "occaecati a doloribus\niste saepe consectetur placeat eum voluptate dolorem et\nqui quo quia voluptas\nrerum ut id enim velit est perferendis",
@@ -208,7 +209,7 @@ const dummyPosts = [
   },
   {
     userId: 8,
-    id: 72,
+    id: 17,
     title: "sint hic doloribus consequatur eos non id",
     body:
       "quam occaecati qui deleniti consectetur\nconsequatur aut facere quas exercitationem aliquam hic voluptas\nneque id sunt ut aut accusamus\nsunt consectetur expedita inventore velit",
@@ -216,7 +217,7 @@ const dummyPosts = [
   },
   {
     userId: 8,
-    id: 73,
+    id: 18,
     title: "consequuntur deleniti eos quia temporibus ab aliquid at",
     body:
       "voluptatem cumque tenetur consequatur expedita ipsum nemo quia explicabo\naut eum minima consequatur\ntempore cumque quae est et\net in consequuntur voluptatem voluptates aut",
@@ -224,7 +225,7 @@ const dummyPosts = [
   },
   {
     userId: 9,
-    id: 88,
+    id: 19,
     title: "sapiente omnis fugit eos",
     body:
       "consequatur omnis est praesentium\nducimus non iste\nneque hic deserunt\nvoluptatibus veniam cum et rerum sed",
@@ -232,7 +233,7 @@ const dummyPosts = [
   },
   {
     userId: 9,
-    id: 89,
+    id: 20,
     title: "sint soluta et vel magnam aut ut sed qui",
     body:
       "repellat aut aperiam totam temporibus autem et\narchitecto magnam ut\nconsequatur qui cupiditate rerum quia soluta dignissimos nihil iure\ntempore quas est",
@@ -240,7 +241,7 @@ const dummyPosts = [
   },
   {
     userId: 9,
-    id: 90,
+    id: 21,
     title: "ad iusto omnis odit dolor voluptatibus",
     body:
       "minus omnis soluta quia\nqui sed adipisci voluptates illum ipsam voluptatem\neligendi officia ut in\neos soluta similique molestias praesentium blanditiis",
@@ -248,7 +249,7 @@ const dummyPosts = [
   },
   {
     userId: 10,
-    id: 91,
+    id: 22,
     title: "aut amet sed",
     body:
       "libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat",
@@ -256,7 +257,7 @@ const dummyPosts = [
   },
   {
     userId: 10,
-    id: 92,
+    id: 23,
     title: "ratione ex tenetur perferendis",
     body:
       "aut et excepturi dicta laudantium sint rerum nihil\nlaudantium et at\na neque minima officia et similique libero et\ncommodi voluptate qui",
@@ -264,7 +265,7 @@ const dummyPosts = [
   },
   {
     userId: 10,
-    id: 93,
+    id: 24,
     title: "beatae soluta recusandae",
     body:
       "dolorem quibusdam ducimus consequuntur dicta aut quo laboriosam\nvoluptatem quis enim recusandae ut sed sunt\nnostrum est odit totam\nsit error sed sunt eveniet provident qui nulla",
@@ -272,7 +273,7 @@ const dummyPosts = [
   },
   {
     userId: 10,
-    id: 94,
+    id: 25,
     title: "qui qui voluptates illo iste minima",
     body:
       "aspernatur expedita soluta quo ab ut similique\nexpedita dolores amet\nsed temporibus distinctio magnam saepe deleniti\nomnis facilis nam ipsum natus sint similique omnis",
@@ -280,7 +281,7 @@ const dummyPosts = [
   },
   {
     userId: 10,
-    id: 95,
+    id: 26,
     title: "id minus libero illum nam ad officiis",
     body:
       "earum voluptatem facere provident blanditiis velit laboriosam\npariatur accusamus odio saepe\ncumque dolor qui a dicta ab doloribus consequatur omnis\ncorporis cupiditate eaque assumenda ad nesciunt",
@@ -288,7 +289,7 @@ const dummyPosts = [
   },
   {
     userId: 10,
-    id: 96,
+    id: 27,
     title: "quaerat velit veniam amet cupiditate aut numquam ut sequi",
     body:
       "in non odio excepturi sint eum\nlabore voluptates vitae quia qui et\ninventore itaque rerum\nveniam non exercitationem delectus aut",
@@ -296,41 +297,133 @@ const dummyPosts = [
   }
 ];
 
+const dummyComments = [
+  {
+    id: 1,
+    authorId: 1,
+    post: 10,
+    commentText:
+      "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+  },
+  {
+    id: 2,
+    authorId: 4,
+    post: 9,
+    commentText:
+      "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+  },
+  {
+    id: 3,
+    authorId: 2,
+    post: 8,
+    commentText:
+      "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+  },
+  {
+    id: 4,
+    authorId: 3,
+    post: 7,
+    commentText:
+      "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+  },
+  {
+    id: 5,
+    authorId: 5,
+    post: 6,
+    commentText:
+      "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+  },
+  {
+    id: 6,
+    authorId: 8,
+    post: 5,
+    commentText:
+      "doloribus at sed quis culpa deserunt consectetur qui praesentium\naccusamus fugiat dicta\nvoluptatem rerum ut voluptate autem\nvoluptatem repellendus aspernatur dolorem in"
+  },
+  {
+    id: 7,
+    authorId: 8,
+    post: 4,
+    commentText:
+      "maiores sed dolores similique labore et inventore et\nquasi temporibus esse sunt id et\neos voluptatem aliquam\naliquid ratione corporis molestiae mollitia quia et magnam dolor"
+  },
+  {
+    id: 8,
+    authorId: 9,
+    post: 2,
+    commentText:
+      "ut voluptatem corrupti velit\nad voluptatem maiores\net nisi velit vero accusamus maiores\nvoluptates quia aliquid ullam eaque"
+  },
+  {
+    id: 9,
+    authorId: 10,
+    post: 1,
+    commentText:
+      "sapiente assumenda molestiae atque\nadipisci laborum distinctio aperiam et ab ut omnis\net occaecati aspernatur odit sit rem expedita\nquas enim ipsam minus"
+  },
+  {
+    id: 10,
+    authorId: 5,
+    post: 1,
+    commentText:
+      "voluptate iusto quis nobis reprehenderit ipsum amet nulla\nquia quas dolores velit et non\naut quia necessitatibus\nnostrum quaerat nulla et accusamus nisi facilis"
+  }
+];
+
 // Type Definitions [schema]
 const typeDefs = `
+
+  # Query is a type in GraphQL for fetching Data
   type Query {
-    users(searchQuery: String): [User!]!
-    me: User!
-    post: Post!
     posts(searchQuery: String): [Posts!]!
+    users(searchQuery: String): [User!]!
+    comments: [Comments!]!
   }
 
-  type User {
+  # Mutation is a type in GraphQL for Create, Update, Delete
+  type Mutation {
+    createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, userId: ID!): Posts!
+    createComment(commentText: String!, post: ID!, authorId: ID!): Comments!
+  }
+
+  type Comments {
     id: ID!
-    name: String!
-    email: String!
-    age: Int
+    commentText: String!
+    author: [User!]!
+    posts: Posts!
   }
-
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-  }
-
+  
   type Posts {
     userId: ID!
     id: ID!
     title: String!
     body: String!
     published: Boolean
+    author: User!
+    comments: [Comments]!
+
   }
-`;
+
+  #This is to retrive the user info
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+    posts: [Posts!]!
+    comments: [Comments!]!
+  }
+  `;
 
 // Resolvers
 const resolvers = {
   Query: {
+    comments() {
+      return dummyComments;
+    },
+
+    // All User Info Data
     users(parent, args, ctx, info) {
       if (!args.searchQuery) {
         return dummyUsers;
@@ -339,23 +432,17 @@ const resolvers = {
         return user.name.toLowerCase().includes(args.searchQuery.toLowerCase());
       });
     },
-    me() {
-      return {
-        id: Math.floor(Math.random() * 9999),
-        name: "Code Resolvers",
-        email: "kiran.ray99@gmail.com",
-        age: 25
-      };
-    },
 
-    post() {
-      return {
-        id: Math.floor(Math.random() * 9999),
-        title: "How GraphQL works under the hood",
-        body: "How to fit into the frameworks with the GraphQL Technology",
-        published: true
-      };
-    },
+    // post() {
+    //   return {
+    //     id: Math.floor(Math.random() * 9999),
+    //     title: "How GraphQL works under the hood",
+    //     body: "How to fit into the frameworks with the GraphQL Technology",
+    //     published: true
+    //   };
+    // },
+
+    // All Posts Info Data
     posts(parent, args, ctx, info) {
       if (!args.searchQuery) {
         return dummyPosts;
@@ -368,6 +455,110 @@ const resolvers = {
           .toLowerCase()
           .includes(args.searchQuery.toLowerCase());
         return isTitleMatch || isPostBodyMatch;
+      });
+    }
+  },
+
+  // Mutations
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = dummyUsers.some(user => {
+        return user.email === args.email;
+      });
+      if (emailTaken) {
+        throw new Error("Email is already taken");
+      }
+      const user = {
+        id: uuidv4(),
+        name: args.name,
+        email: args.email,
+        age: args.age
+      };
+
+      dummyUsers.push(user);
+      return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = dummyUsers.some(user => {
+        return user.id === args.userId;
+      });
+
+      if (!userExists) {
+        throw new Error("User not found");
+      }
+
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        userId: args.userId
+      };
+
+      dummyPosts.push(post);
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = dummyUsers.some(user => {
+        return user.id === args.authorId;
+      });
+
+      const postExists = dummyPosts.find(post => {
+        return post.id === args.post;
+      });
+
+      if (!userExists || !postExists) {
+        throw new Error("User/Post Not Found");
+      }
+
+      const comment = {
+        id: uuidv4(),
+        commentText: args.commentText,
+        authorId: args.authorId,
+        post: args.post
+      };
+      dummyComments.push(comment);
+      return comment;
+    }
+  },
+
+  // Getting Individual user type data in posts for fetching the author userId this is outside of the Query global property
+
+  Comments: {
+    author(parent, args, ctx, info) {
+      return dummyUsers.filter(user => {
+        return user.id === parent.authorId;
+      });
+    },
+    posts(parent, args, ctx, info) {
+      return dummyPosts.find(post => {
+        return post.id === parent.post;
+      });
+    }
+  },
+
+  Posts: {
+    author(parent, args, ctx, info) {
+      return dummyUsers.find(user => {
+        // Here parent argument refers to the dummyposts array objects of each for every iteration
+        return user.id === parent.userId;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return dummyComments.filter(comment => {
+        return comment.post === parent.id;
+      });
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return dummyPosts.filter(post => {
+        return post.userId === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return dummyComments.filter(comment => {
+        return comment.authorId === parent.id;
       });
     }
   }
